8074fefc607e29b720464e80964ed5936084ac00
Minat Silvester <minatsilvester@gmail.com>
2025-03-03 15:56:02 +0530

    feta: added cancel for focuss ession

--- lib/deep_work/limelight.ex
+++ lib/deep_work/limelight.ex
@@ -93,17 +93,17 @@ defmodule DeepWork.Limelight do
     Stop stop a active focus session
     Updates the focus session's end_time
   """
-  def stop_focus_sessions(%FocusSessions{} = focus_sessions, user) do
+  def stop_focus_sessions(%FocusSessions{} = focus_sessions, attrs, user) do
     end_time = get_current_naive_datetime(user)
     update_focus_sessions(
       focus_sessions,
-      %{}
+      attrs
       |> Map.put("end_time", end_time)
       |> Map.put("actual_length",
-        get_difference_in_naive_date_time(end_time, focus_sessions.start_time) / 60)
-      |> Map.put("status", "completed")
-    )
+        div(get_difference_in_naive_date_time(end_time, focus_sessions.start_time),60)))
   end
+
+
 
   @doc """
   Updates a focus_sessions.
